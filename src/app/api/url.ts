49846b1728b401cdbac87e7f99132cb2f7cc1307
3str-webdev <3str.webdev@gmail.com>
2023-03-24 08:21:26 +0300

    fix: add prod server url

--- src/app/api/url.ts
+++ src/app/api/url.ts
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://localhost:4001/';
+export const baseUrl = 'https://game-tasks-server.onrender.com:4001/';
 export const apiUrl = `${baseUrl}api/`;
 export const getAllUrl = `${apiUrl}tasks/getAll`;
 export const getById = `${apiUrl}tasks/get/`; // + taskId
